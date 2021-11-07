7ff49b8e1e954c00328891b2e3b8c9cab6bfbdbb
abcdef <mattkerschbaum@Hotmail.com>
2021-11-06 17:37:17 -0700

    Added setup to allow importing

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # goopyconfig
 Configuration module based on the configObj library
+
+NOTE: not currently uploaded to PyPi. To install module so it can be imported:
+   1) clone the repo
+   2) cd to the cloned folder (where the setup.py file is located)
+   3) type 'pip install .'
---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(name='goopyconfig',
+version='1.0',
+description='Goo configuration module based on configObj library',
+url='#',
+author='max',
+install_requires=['configobj', 'loguru'],
+author_email='',
+packages=setuptools.find_packages(),
+zip_safe=False)
